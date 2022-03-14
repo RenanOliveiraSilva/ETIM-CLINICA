61f46e6cb1224fca8d32e1fd37fb29e9ec0aabf9
RenanOliveiraSilva <renandeoliveirasilva123@gmail.com>
2022-03-14 11:32:53 -0300

    Aula 1

---
+++ conexao/conexao.php
@@ -0,0 +1,28 @@
+<?php
+
+    Class Conexao{
+        private $host = 'localhost';
+        private $dbname = 'clinica';
+        private $user = 'root';
+        private $senha = '';
+
+        public function conectar() {
+            try {
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbname=$this->dbname",
+                    "$this->user",
+                    "$this->senha"
+                );
+                return $conexao;
+
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }
+        }
+
+    }
+
+    $con = new Conexao();
+    $con->conectar();
+
+?>
